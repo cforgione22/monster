f2b1c45ddd6e1728b254bdc76a0504c9ef31a79e
cforgione22 <crystal.forgione22@gmail.com>
2024-08-01 16:41:51 +0000

    monster, base class

---
+++ main.cpp
@@ -0,0 +1,12 @@
+//Crystal Forgione
+//FINAL
+//24.SU.CIS.1202.201
+//08-04-24
+
+#include "importLibrary.h"
+
+int main() {
+
+  
+  return 0;
+}
---
+++ monster.cpp
@@ -0,0 +1,6 @@
+//Crystal Forgione
+//FINAL
+//24.SU.CIS.1202.201
+//08-04-24
+#include "importLibrary.h"
+
