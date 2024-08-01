3c9b666d41eb861dbb4aec9bb0799f50b02e4ca2
cforgione22 <crystal.forgione22@gmail.com>
2024-08-01 17:27:15 +0000

    created monster class!

--- monster.cpp
+++ monster.cpp
@@ -4,3 +4,24 @@
 //08-04-24
 #include "importLibrary.h"
 
+class Monster 
+{
+  private:
+    string food;
+    string sex;
+    string weakness;
+    string defense;
+    string uniqueCharacteristic;
+    double height;
+  pubic:
+    Monster();
+    void setFood(string);
+    void setSex(string);
+    void setWeakness(string);
+    void setDefense(string);
+    void setUniqueCharacteristic(string);
+    void setHeight(double);
+    void displayInfor();
+};
+
+
---
+++ monster.h
@@ -0,0 +1,48 @@
+//Crystal Forgione
+//FINAL
+//24.SU.CIS.1202.201
+//08-04-24
+#include "importLibrary.h"
+#include "monster.h"
+
+Monster::Monster() {}
+
+void Monster::setFood(string fd)
+{
+  food = fd;
+}
+
+void Monster::setSex(string sx)
+{
+  sex = sx;
+}
+
+void Monster::setWeakness(string weak)
+{
+  weakness = weak;
+}
+
+void Monster::setDefense(string def)
+{
+  defense = def;
+}
+
+void Monster::setUniqueCharacteristic(string uniqueChar)
+{
+  uniqueCharacteristic = uniqueChar;
+}
+
+void Monster::setHeight( double ht)
+{
+  height = ht;
+}
+
+void Monster::displayInfor()
+{
+  food
+  sex
+  weakness
+  defense
+  uniqueCharacteristic
+  height 
+}
